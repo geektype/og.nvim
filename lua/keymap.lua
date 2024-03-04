1786887d49646cb9a0f775ee409a946cb3a80205
Abdullah Ahmed <12223088+geektype@users.noreply.github.com>
2024-03-04 19:29:58 +0000

    Split into files

---
+++ lua/keymap.lua
@@ -0,0 +1,3 @@
+vim.g.mapleader = ' '
+
+vim.keymap.set({'n'}, '<leader>q', ':NvimTreeToggle<CR>')
---
+++ lua/options.lua
@@ -0,0 +1,20 @@
+-- Hybrid Line Numbering
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = "a"
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+
+-- Tab/spacing
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.termguicolors = true
+vim.opt.wrap = false
