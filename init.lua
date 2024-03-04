a3973492b7e01b3b4a90cc6509b9fcd82eba7d23
Abdullah Ahmed <12223088+geektype@users.noreply.github.com>
2024-03-04 21:20:14 +0000

    Add LSP

--- init.lua
+++ init.lua
@@ -8,6 +8,47 @@ require("keymap")
 require("lazy-nvim")
 
 require("nvim-tree").setup()
+
+
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {'lua_ls', 'clangd'},
+  handlers = {
+    lsp_zero.default_setup,
+    require'lspconfig'.lua_ls.setup {
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+          client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+            Lua = {
+              runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT'
+              },
+              -- Make the server aware of Neovim runtime files
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME
+                }
+              }
+            }
+          })
+        end
+        return true
+      end
+    }
+  },
+})
+
+
 require("bufferline").setup{
     options = {
         offsets = {
--- lua/lazy-nvim.lua
+++ lua/lazy-nvim.lua
@@ -59,6 +59,13 @@ require("lazy").setup({
     'windwp/nvim-autopairs',
     event = "InsertEnter",
     config = true
-    }
+    },
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
 
 })
