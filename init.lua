3148c067c99144ed899f48a17c86104748a37f73
Abdullah Ahmed <12223088+geektype@users.noreply.github.com>
2024-03-07 21:33:04 +0000

    LSP refactor

--- init.lua
+++ init.lua
@@ -9,55 +9,9 @@ require("lazy-nvim")
 
 require("nvim-tree").setup()
 
-local lsp_zero = require('lsp-zero')
+require("lsp")
 
-lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
-
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {'lua_ls', 'clangd'},
-  handlers = {
-    lsp_zero.default_setup,
-    require'lspconfig'.lua_ls.setup {
-      on_init = function(client)
-        local path = client.workspace_folders[1].name
-        if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
-          client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-            Lua = {
-              runtime = {
-                -- Tell the language server which version of Lua you're using
-                -- (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT'
-              },
-              -- Make the server aware of Neovim runtime files
-              workspace = {
-                checkThirdParty = false,
-                library = {
-                  vim.env.VIMRUNTIME
-                }
-              }
-            }
-          })
-        end
-        return true
-      end
-    }
-  },
-})
-
-local cmp = require('cmp')
-
-cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
-    ['<S-Tab>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-    ['<Tab>'] = cmp.mapping.select_next_item({behavior = 'select'}),
-  }),
-})
-
+vim.test()
 
 require("bufferline").setup{
     options = {
---
+++ lua/lsp.lua
@@ -0,0 +1,45 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {'lua_ls', 'clangd'},
+  handlers = {
+    lsp_zero.default_setup,
+    require'lspconfig'.lua_ls.setup {
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+          client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+            Lua = {
+              runtime = {
+                version = 'LuaJIT'
+              },
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME
+                }
+              }
+            }
+          })
+        end
+        return true
+      end
+    }
+  },
+})
+
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+    ['<Tab>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+  }),
+})
