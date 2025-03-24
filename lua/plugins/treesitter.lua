e59bf2cb892874bc4a3f1af86c21921396d83417
Abdullah Ahmed <12223088+geektype@users.noreply.github.com>
2025-03-24 13:52:40 +0000

    Add typescript and zig support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function ()
-      local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "go"},
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true }
-        })
-    end
- }
+		configs.setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "go", "javascript", "typescript", "zig" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
