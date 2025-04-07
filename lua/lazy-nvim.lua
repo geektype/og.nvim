88bcdc53760bf257f057dadee050afda2df510df
Abdullah Ahmed <12223088+geektype@users.noreply.github.com>
2025-04-07 22:11:14 +0100

    Voodoo

--- lua/lazy-nvim.lua
+++ lua/lazy-nvim.lua
@@ -1,6 +1,5 @@
 require("lazy").setup({
 	{ import = "themes" },
-	{ import = "plugins" },
 	"christoomey/vim-tmux-navigator",
 	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 	{ "windwp/nvim-autopairs", event = "InsertEnter", config = true },
@@ -12,4 +11,6 @@ require("lazy").setup({
 	{ "hrsh7th/nvim-cmp" },
 	{ "L3MON4D3/LuaSnip" },
 	{ "lewis6991/gitsigns.nvim", config = true },
+	-- Investigate: For some reason placing this anywhere but here slows neovim down to a crawl...
+	{ import = "plugins" },
 })
